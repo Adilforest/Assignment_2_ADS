8e50b84bb89cea296e0145c5626873eb6c0853ac
Adil Ormanov <>
2024-04-24 08:50:34 +0500

    MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         testList();
-
     }
 
     private static void testList() {
         testAdd(new MyArrayList<>());
-
 
         testDelete(new MyArrayList<>());
 
@@ -28,24 +26,24 @@ public class Main {
         testExists(new MyArrayList<>());
     }
 
-    private static void testAdd(MyList<Integer> impl) {
+    private static void testAdd(MyList<Integer> n) {
         Integer[] arr = {0, 1, 2, 3, 4, 5};
         for (int i = 1; i <= 5; i++)
-            impl.add(i);
-        impl.addFirst(0);
-        System.out.println(Arrays.equals(arr, impl.toArray()));
+            n.add(i);
+        n.addFirst(0);
+        System.out.println(Arrays.equals(arr, n.toArray()));
     }
 
-    private static void testDelete(MyList<Integer> impl) {
+    private static void testDelete(MyList<Integer> n) {
         Integer[] arr = {1, 2, 3, 5, 6};
         for (int i = 1; i <= 6; i++)
-            impl.add(i);
-        impl.add(5);
-        impl.removeLast();
-        impl.remove(3);
-        boolean res = Arrays.equals(arr, impl.toArray());
+            n.add(i);
+        n.add(5);
+        n.removeLast();
+        n.remove(3);
+        boolean res = Arrays.equals(arr, n.toArray());
         if (!res)
-            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(impl.toArray()));
+            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(n.toArray()));
         else
             System.out.println(res);
     }
@@ -64,46 +62,46 @@ public class Main {
         System.out.println(res);
     }
 
-    private static void testSet(MyList<Integer> impl) {
+    private static void testSet(MyList<Integer> n) {
         Integer[] arr = {1, 2, 5, 4, 5};
         for (int i = 1; i <= 5; i++)
-            impl.add(i);
+            n.add(i);
 
-        impl.set(4, 2);
+        n.set(4, 2);
 
-        boolean res = Arrays.equals(arr, impl.toArray());
+        boolean res = Arrays.equals(arr, n.toArray());
         if (!res)
-            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(impl.toArray()));
+            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(n.toArray()));
         else
             System.out.println(res);
     }
 
 
 
-    private static void testIndexOf(MyList<Integer> impl) {
+    private static void testIndexOf(MyList<Integer> n) {
         for (int i = 1; i <= 5; i++)
-            impl.add(i);
+            n.add(i);
 
-        boolean res = impl.indexOf(4) == 3;
+        boolean res = n.indexOf(4) == 3;
         System.out.println(res);
     }
 
-    private static void testLastIndexOf(MyList<Integer> impl) {
+    private static void testLastIndexOf(MyList<Integer> n) {
         for (int i = 0; i < 2; i++) {
-            impl.add(1);
-            impl.add(2);
-            impl.add(3);
+            n.add(1);
+            n.add(2);
+            n.add(3);
         }
 
-        boolean res = impl.lastIndexOf(2) == 4;
+        boolean res = n.lastIndexOf(2) == 4;
         System.out.println(res);
     }
 
-    private static void testExists(MyList<Integer> impl) {
+    private static void testExists(MyList<Integer> n) {
         for (int i = 0; i < 1000; i++)
-            impl.add(i);
+            n.add(i);
 
-        boolean res = impl.exists(500);
+        boolean res = n.exists(500);
         System.out.println(res);
     }
 
