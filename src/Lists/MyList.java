a83b8a17dbf7a5453d6cfa49007830d90255a3db
Adil Ormanov <>
2024-04-24 05:21:30 +0500

    MyArrayList

---
+++ src/Lists/MyList.java
@@ -0,0 +1,22 @@
+package Lists;
+
+public interface MyList<T> extends Iterable<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    int indexOf(T object);
+    boolean exists(T object);
+    T[] toArray();
+    void clear();
+    int size();
+
+    int lastIndexOf(T item);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,110 @@
+import Lists.MyArrayList;
+import Lists.MyLinkedList;
+import Lists.MyList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        testList();
 
-        for (int i = 1; i <= 5; i++) {
-fdklsnfeklsdnfvsd
-        dsfnjksdlfnsddfsmklsdv
-        dsfnsdklf
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+    private static void testList() {
+        testAdd(new MyArrayList<>());
+
+
+        testDelete(new MyArrayList<>());
+
+        testGet(new MyArrayList<>());
+
+        testSet(new MyArrayList<>());
+
+        testIndexOf(new MyArrayList<>());
+
+        testLastIndexOf(new MyArrayList<>());
+
+        testExists(new MyArrayList<>());
+    }
+
+    private static void testAdd(MyList<Integer> impl) {
+        Integer[] arr = {0, 1, 2, 3, 4, 5};
+        for (int i = 1; i <= 5; i++)
+            impl.add(i);
+        impl.addFirst(0);
+        System.out.println(Arrays.equals(arr, impl.toArray()));
+    }
+
+    private static void testDelete(MyList<Integer> impl) {
+        Integer[] arr = {1, 2, 3, 5, 6};
+        for (int i = 1; i <= 6; i++)
+            impl.add(i);
+        impl.add(5);
+        impl.removeLast();
+        impl.remove(3);
+        boolean res = Arrays.equals(arr, impl.toArray());
+        if (!res)
+            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(impl.toArray()));
+        else
+            System.out.println(res);
+    }
+    private static void testGet(MyList<Integer> impl) {
+        for (int i = 0; i < 10; i++)
+            impl.add(i);
+
+        boolean res = true;
+        for (int i = 0; i < 10; i++) {
+            Integer value = impl.get(i);
+            if (value == null || !value.equals(i)) {
+                res = false;
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+
+    private static void testSet(MyList<Integer> impl) {
+        Integer[] arr = {1, 2, 5, 4, 5};
+        for (int i = 1; i <= 5; i++)
+            impl.add(i);
+
+        impl.set(4, 2);
+
+        boolean res = Arrays.equals(arr, impl.toArray());
+        if (!res)
+            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(impl.toArray()));
+        else
+            System.out.println(res);
+    }
+
+
+
+    private static void testIndexOf(MyList<Integer> impl) {
+        for (int i = 1; i <= 5; i++)
+            impl.add(i);
+
+        boolean res = impl.indexOf(4) == 3;
+        System.out.println(res);
+    }
+
+    private static void testLastIndexOf(MyList<Integer> impl) {
+        for (int i = 0; i < 2; i++) {
+            impl.add(1);
+            impl.add(2);
+            impl.add(3);
+        }
+
+        boolean res = impl.lastIndexOf(2) == 4;
+        System.out.println(res);
+    }
+
+    private static void testExists(MyList<Integer> impl) {
+        for (int i = 0; i < 1000; i++)
+            impl.add(i);
+
+        boolean res = impl.exists(500);
+        System.out.println(res);
+    }
+
 }
