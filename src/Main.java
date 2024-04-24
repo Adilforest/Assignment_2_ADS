259bf227e1e377dd169f13d7fb01b00ce29d470d
Adil Ormanov <>
2024-04-24 09:19:44 +0500

    MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -7,102 +7,33 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        testList();
+        MyArrayList<Integer> n = new MyArrayList<>();
+        testAddMethods(n);
+        testGetMethods(n);
     }
 
-    private static void testList() {
-        testAdd(new MyArrayList<>());
+    private static void testAddMethods(MyArrayList<Integer> n){
+        n.add(1);
+        n.addFirst(2);
+        n.addLast(3);
+        n.add(4, 3);
+        n.addLast(5);
 
-        testDelete(new MyArrayList<>());
-
-        testGet(new MyArrayList<>());
-
-        testSet(new MyArrayList<>());
-
-        testIndexOf(new MyArrayList<>());
-
-        testLastIndexOf(new MyArrayList<>());
-
-        testExists(new MyArrayList<>());
+        System.out.println("adding " + Arrays.toString(n.toArray()));
     }
-
-    private static void testAdd(MyList<Integer> n) {
-        Integer[] arr = {0, 1, 2, 3, 4, 5};
-        for (int i = 1; i <= 5; i++)
+    private static void testGetMethods(MyList<Integer> n) {
+        for (int i = 1; i <= 5; i++) {
             n.add(i);
-        n.addFirst(0);
-        System.out.println(Arrays.equals(arr, n.toArray()));
-    }
-
-    private static void testDelete(MyList<Integer> n) {
-        Integer[] arr = {1, 2, 3, 5, 6};
-        for (int i = 1; i <= 6; i++)
-            n.add(i);
-        n.add(5);
-        n.removeLast();
-        n.remove(3);
-        boolean res = Arrays.equals(arr, n.toArray());
-        if (!res)
-            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(n.toArray()));
-        else
-            System.out.println(res);
-    }
-    private static void testGet(MyList<Integer> impl) {
-        for (int i = 0; i < 10; i++)
-            impl.add(i);
-
-        boolean res = true;
-        for (int i = 0; i < 10; i++) {
-            Integer value = impl.get(i);
-            if (value == null || !value.equals(i)) {
-                res = false;
-                break;
-            }
         }
-        System.out.println(res);
+        Integer getElement = n.get(0);
+        Integer getFirstElement = n.getFirst();
+        Integer getLastElement = n.getLast();
+
+
+        System.out.println("index 3: " + getElement);
+        System.out.println("First element: " + getFirstElement);
+        System.out.println("Last element: " + getLastElement);
     }
 
-    private static void testSet(MyList<Integer> n) {
-        Integer[] arr = {1, 2, 5, 4, 5};
-        for (int i = 1; i <= 5; i++)
-            n.add(i);
-
-        n.set(4, 2);
-
-        boolean res = Arrays.equals(arr, n.toArray());
-        if (!res)
-            System.out.print(Arrays.toString(arr) + " | " + Arrays.toString(n.toArray()));
-        else
-            System.out.println(res);
-    }
-
-
-
-    private static void testIndexOf(MyList<Integer> n) {
-        for (int i = 1; i <= 5; i++)
-            n.add(i);
-
-        boolean res = n.indexOf(4) == 3;
-        System.out.println(res);
-    }
-
-    private static void testLastIndexOf(MyList<Integer> n) {
-        for (int i = 0; i < 2; i++) {
-            n.add(1);
-            n.add(2);
-            n.add(3);
-        }
-
-        boolean res = n.lastIndexOf(2) == 4;
-        System.out.println(res);
-    }
-
-    private static void testExists(MyList<Integer> n) {
-        for (int i = 0; i < 1000; i++)
-            n.add(i);
-
-        boolean res = n.exists(500);
-        System.out.println(res);
-    }
 
 }
